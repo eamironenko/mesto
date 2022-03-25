28dedd6b24c0102baf4c5683e4287939aed6cc0b
unknown <eam618334@yandex.ru>
2022-03-26 00:45:03 +0300

    fix: отладить popup css

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-/*открытие/закрытие Popup*/
+//*открытие/закрытие Popup*/
 
 const popupElement = document.querySelector('.popup');
 const navButton = document.querySelector('.profile__edit-button');
@@ -7,17 +7,19 @@ const ESC_KEY = "Escape";
 const formElement = document.querySelector('.popup__content');
 const currentName = document.querySelector('.profile__name');
 const currentProf = document.querySelector('.profile__subtitle');
-const inputName = formElement.querySelector('.popup__form-name');
-const inputProf = formElement.querySelector('.popup__form-profession');
+const inputName = formElement.querySelector('.popup__input_type_name');
+const inputProf = formElement.querySelector('.popup__input_type_profession');
+const saveButton = document.querySelector('.popup__save-button');
 
 function openPopup() {
     popupElement.classList.add('popup_opened');
-    document.addEventListener('keyup', onDocumentKeyUp)
+    document.addEventListener('keyup', onDocumentKeyUp);
 }
 
 function closePopup() {
     popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keyup', onDocumentKeyUp)
+    document.removeEventListener('keyup', onDocumentKeyUp);
+    formElement.classList.remove('popup_opened');
 }
 
 function onDocumentKeyUp(event){
@@ -31,13 +33,14 @@ closeButton.addEventListener('click', closePopup);
 
 /*Ввод данных и сохранение формы*/
 
+inputName.value = "Жак-Ив Кусто";
+inputProf.value = "Исследователь океана";
+
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-
     currentName.textContent = inputName.value;
     currentProf.textContent = inputProf.value;
-    console.log(currentName);
-    console.log(currentProf);
 }
 
 formElement.addEventListener('submit', handleProfileFormSubmit);
+saveButton.addEventListener('click', closePopup);
